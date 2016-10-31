4f6e0a880f3195800d72ed908b45bf8382511bdb
rsgc-noblecurveira-c <student@macbook-pro-carlos-noble-curviera.local>
2016-10-31 08:38:37 -0400

    Reached the end of Connecting the UI to code

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -20,12 +20,20 @@ class ViewController: UIViewController, UITextFieldDelegate {
         // Handle the text field’s user input through delegate callbacks.
         nameTextField.delegate = self
     }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    //MARK: Actions
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+    // MARK: Actions
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         mealNameLabel.text = "Default Text"
     }
