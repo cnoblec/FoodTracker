241f0e1e3a42519aacfc30d6d6b506f9ef8a9a38
rsgc-noblecurveira-c <student@macbook-pro-carlos-noble-curviera.local>
2016-11-04 09:31:57 -0400

    Working end of connecting UI to code

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     // MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
@@ -16,7 +16,18 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self
+    }
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        nameMealLabel.text = textField.text
     }
     // MARK: Actions
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
