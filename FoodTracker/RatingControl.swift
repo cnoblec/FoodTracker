83b4391aa1502e4ae62e37f20213db0a7e2ed03c
rsgc-noblecurveira-c <student@macbook-pro-carlos-noble-curviera.local>
2016-11-04 18:40:33 -0400

    Same as last time with variables instead of random constants

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -10,13 +10,9 @@ import UIKit
 
 class RatingControl: UIView {
     // MARK: Properties
-    
     var rating = 0
-    
     var ratingButtons = [UIButton]()
-    
     let spacing = 5
-    
     let starCount = 5
     
     // MARK: Initialization
@@ -24,7 +20,8 @@ class RatingControl: UIView {
         super.init(coder: aDecoder)
         
         for _ in 0..<starCount {
-            let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+            
+            let button = UIButton()
             button.backgroundColor = UIColor.red
             button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchDown)
             ratingButtons += [button]
@@ -33,11 +30,14 @@ class RatingControl: UIView {
     }
     override public var intrinsicContentSize: CGSize {
         get {
-            return CGSize(width: 240, height: 44)
+            let buttonSize = Int(frame.size.height)
+            let width = (buttonSize * starCount) + (spacing * (starCount - 1))
+            return CGSize(width: width, height: buttonSize)
         }
     }
-    override func layoutSubviews() {
+    override func layoutSubviews() {        
         var buttonFrame = CGRect(x: 0, y: 0, width: 44, height: 44)
+        
         for (index, button) in ratingButtons.enumerated(){
             buttonFrame.origin.x = CGFloat(index * (44 + spacing))
             button.frame = buttonFrame
