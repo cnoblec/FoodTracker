661494931abe28ab5038fd2f7792e9bc78a261a0
rsgc-noblecurveira-c <student@macbook-pro-carlos-noble-curviera.local>
2016-11-04 19:42:11 -0400

    test cases were working. some trouble with the initial fail test case.

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,26 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Carlos Noble Curveira on 2016-11-04.
+//  Copyright © 2016 Carlos Noble Curveira. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    // MARK: Properties
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    // MARK: Initialization
+    init?(name: String, photo: UIImage?, rating: Int) {
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+    }
+}
+
--- FoodTrackerTests/FoodTrackerTests.swift
+++ FoodTrackerTests/FoodTrackerTests.swift
@@ -11,26 +11,16 @@ import XCTest
 
 class FoodTrackerTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    // MARK: Foodtracker tests
+    func testMealInitialization() {
+        // success
+        let potentialMeal = Meal(name: "new", photo: nil, rating: 5)
+        XCTAssertNotNil(potentialMeal)
+        // fail
+        let noName = Meal(name: "", photo: nil, rating: 0)
+        XCTAssertNil(noName, "Empty name is invalid")
+        // fail again
+        let badRating = Meal(name: "bad", photo: nil, rating: -1)
+        XCTAssertNil(badRating, "negative rating is invalid")
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
