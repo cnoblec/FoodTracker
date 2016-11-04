1b6b10be633f4231f87f4efcead8f5eb71b320a7
rsgc-noblecurveira-c <student@macbook-pro-carlos-noble-curviera.local>
2016-11-04 18:20:56 -0400

    Everything working without random constants

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -15,11 +15,15 @@ class RatingControl: UIView {
     
     var ratingButtons = [UIButton]()
     
+    let spacing = 5
+    
+    let starCount = 5
+    
     // MARK: Initialization
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
-        for _ in 0...4 {
+        for _ in 0..<starCount {
             let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
             button.backgroundColor = UIColor.red
             button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchDown)
@@ -35,7 +39,7 @@ class RatingControl: UIView {
     override func layoutSubviews() {
         var buttonFrame = CGRect(x: 0, y: 0, width: 44, height: 44)
         for (index, button) in ratingButtons.enumerated(){
-            buttonFrame.origin.x = CGFloat(index * (44 + 5))
+            buttonFrame.origin.x = CGFloat(index * (44 + spacing))
             button.frame = buttonFrame
         }
     }
