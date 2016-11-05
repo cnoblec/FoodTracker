eb386b3a81a8a695a6300957bc450989890bffab
rsgc-noblecurveira-c <student@macbook-pro-carlos-noble-curviera.local>
2016-11-04 21:17:14 -0400

    Default table rows are input, nothing to display yet

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Carlos Noble Curveira on 2016-11-04.
+//  Copyright © 2016 Carlos Noble Curveira. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
